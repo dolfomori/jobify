06981cd1241e1092350c952e70faf50ea6b39653
Rodolfo <rod_dolpho@hotmail.com>
2019-06-06 11:44:00 -0300

    Fixing Public Directory

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Promise })
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs') // Usar o EJS para renderizar o html
-app.use(express.static('public')) // Conseguir acessar pasta public
+app.use(express.static(path.join(__dirname,'public'))) // Conseguir acessar pasta public
 app.use(bodyParser.urlencoded({ extended : true}))
 
 const port = process.env.PORT || 3000
