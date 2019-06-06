5b294e20a19cb40c29a4946fb4ee77038a81b150
Rodolfo <rod_dolpho@hotmail.com>
2019-06-06 11:18:35 -0300

    Edit PORT

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs') // Usar o EJS para renderizar o html
 app.use(express.static('public')) // Conseguir acessar pasta public
 app.use(bodyParser.urlencoded({ extended : true}))
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', async(req, res) => {
     const db = await dbConnection
